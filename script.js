41d9fbc7459b0685f517a158b1540b60706cd734
David Bernard <dbernardcoding@gmail.com>
2021-03-21 02:37:13 -0400

    updating progress

--- script.js
+++ script.js
@@ -1,85 +1,50 @@
+
 // Global Variables
 let letterCharacter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 let numericCharacter = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 let specialCharacter = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
 let generateBtn = document.querySelector("#generate");
 
+// Global Event Listeners
+generateBtn.addEventListener("click", writePassword); 
+
 // generatePassword Function
 function generatePassword() {
+  let countMessage = prompt("Choose amount of characters between 8 and 128"); 
+  let numericConfirm = confirm("Do you want to include numeric characters?"); 
+  let specialConfirm = confirm("Do you want to include special characters?");
   let combineNumeric = letterCharacter.concat(numericCharacter);
   let combineNumericSpecial = letterCharacter.concat(specialCharacter);
-  let combineSpecial = letterCharacter.concat(numericCharacter, specialCharacter);
-  let numericConfirm = confirm("Do you want to include numeric characters?");
-  let specialConfirm = confirm("Do you want to include special characters?");  
+  let combineSpecial = letterCharacter.concat(numericCharacter, specialCharacter); 
+  
+  while (countMessage < 8) {
+    alert("Must have at least 8 characters");
+    return;
+    
+  } if (countMessage > 128) {
+    alert("Must have no more than 128 characters");
+    return;
+  }
 
   if (numericConfirm === true && specialConfirm === true) {
-    combineSpecial;
+    return combineSpecial;
   }
 
   if (numericConfirm === true && specialConfirm === false) {
-    combineNumeric;
+    return combineNumeric;
   }
 
   if (numericConfirm === false && specialConfirm === true) {
-    combineNumericSpecial;
+    return combineNumericSpecial;
   }
 
   if (numericConfirm === false && specialConfirm === false)
-    letterCharacter;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* // Assignment Code
-var generateBtn = document.querySelector("#generate");
+    return letterCharacter;
+  }
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
   passwordText.value = password;
-
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); */
