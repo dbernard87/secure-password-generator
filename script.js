5fc74d0b198afa3d394d49a3f5dfe6b891163cb8
David Bernard <dbernardcoding@gmail.com>
2021-03-21 04:04:58 -0400

    updating if statements

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 // Global Variables
-let letterCharacter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let upperCharacter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+let lowerCharacter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 let numericCharacter = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 let specialCharacter = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
 let generateBtn = document.querySelector("#generate");
@@ -11,36 +12,60 @@ generateBtn.addEventListener("click", writePassword);
 // generatePassword Function
 function generatePassword() {
   let countMessage = prompt("Choose amount of characters between 8 and 128"); 
+  let upperConfirm = confirm("Do you want to include uppercase characters?");
   let numericConfirm = confirm("Do you want to include numeric characters?"); 
   let specialConfirm = confirm("Do you want to include special characters?");
-  let combineNumeric = letterCharacter.concat(numericCharacter);
-  let combineNumericSpecial = letterCharacter.concat(specialCharacter);
-  let combineSpecial = letterCharacter.concat(numericCharacter, specialCharacter); 
+  let combineAll = lowerCharacter.concat(upperCharacter, numericCharacter, specialCharacter);
+  let combineUpperNumeric = lowerCharacter.concat(upperCharacter, numericCharacter);
+  let combineUpper = lowerCharacter.concat(upperCharacter)
+  let combineNumeric = lowerCharacter.concat(numericCharacter);
+  let combineNumericSpecial = lowerCharacter.concat(specialCharacter);
+  let combineSpecial = lowerCharacter.concat(numericCharacter, specialCharacter); 
   
   while (countMessage < 8) {
     alert("Must have at least 8 characters");
     return;
-    
-  } if (countMessage > 128) {
+  } 
+  
+  if (countMessage > 128) {
     alert("Must have no more than 128 characters");
     return;
+  }
+
+  if (upperConfirm === true && numericConfirm === true && specialConfirm === true) {
+    return combineAll;
+  }
+
+  if (upperConfirm === true && numericConfirm === true) {
+    return combineUpperNumeric;
+  }
+
+  if (upperConfirm === true) {
+    return combineUpper;
   }
 
   if (numericConfirm === true && specialConfirm === true) {
     return combineSpecial;
   }
 
-  if (numericConfirm === true && specialConfirm === false) {
-    return combineNumeric;
-  }
-
-  if (numericConfirm === false && specialConfirm === true) {
+  if (numericConfirm === true) {
     return combineNumericSpecial;
   }
 
-  if (numericConfirm === false && specialConfirm === false)
-    return letterCharacter;
+  if (specialCharacter === true) {
+    return combineNumericSpecial;
   }
+
+  if (upperConfirm === false && numericConfirm === false && specialConfirm === false) {
+    return lowerCharacter;
+  }
+
+  if (specialConfirm === true && upperConfirm === false && numericCharacter === false) {
+    return combineNumericSpecial;
+  }
+
+
+};
 
 // Write password to the #password input
 function writePassword() {
