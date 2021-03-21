27a5e53726d115142370bc1cdc7b07c03b69e3ea
David Bernard <dbernardcoding@gmail.com>
2021-03-21 19:09:03 -0400

    fixing variable names

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 // Global Variables
-let upperCharacter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-let lowerCharacter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-let numericCharacter = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+let upperCharacter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let lowerCharacter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let numericCharacter = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let specialCharacter = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
 let generateBtn = document.querySelector("#generate");
 let combineAll = lowerCharacter.concat(upperCharacter, numericCharacter, specialCharacter);
 let combineUpperNumeric = lowerCharacter.concat(upperCharacter, numericCharacter);
-let combineUpper = lowerCharacter.concat(upperCharacter)
+let combineUpper = lowerCharacter.concat(upperCharacter);
 let combineNumeric = lowerCharacter.concat(numericCharacter);
-let combineNumericSpecial = lowerCharacter.concat(specialCharacter);
-let combineSpecial = lowerCharacter.concat(numericCharacter, specialCharacter); 
-let lowerConfirm = ""; 
-let upperConfirm = "";
-let numericConfirm = ""; 
-let specialConfirm = ""; 
+let combineSpecial = lowerCharacter.concat(specialCharacter);
+let combineNumericSpecial = lowerCharacter.concat(numericCharacter, specialCharacter); 
+let lowerConf = ""; 
+let upperConf = "";
+let numericConf = ""; 
+let specialConf = ""; 
 
 // Global Event Listeners
 generateBtn.addEventListener("click", writePassword); 
 generateBtn.addEventListener("click", characterLength); 
-
 
 // Character Amount Function
 function characterLength() {
@@ -36,49 +35,45 @@ function characterLength() {
 };
 
 function generatePassword() {
-    lowerConfirm = confirm("Do you want to include lowercase characters?");
-    upperConfirm = confirm("Do you want to include uppercase characters?");
-    numericConfirm = confirm("Do you want to include numeric characters?");
-    specialConfirm = confirm("Do you want to include special characters?");
+    lowerConf = confirm("Do you want to include lowercase characters?");
+    upperConf = confirm("Do you want to include uppercase characters?");
+    numericConf = confirm("Do you want to include numeric characters?");
+    specialConf = confirm("Do you want to include special characters?");
 
-
-    if (upperConfirm === true && numericConfirm === true && specialConfirm === true) {
+    if (upperConf && numericConf  && specialConf) {
       return combineAll;
     }
   
-    else if (upperConfirm === true && numericConfirm === true) {
+    else if (upperConf && numericConf && !specialConf) {
       return combineUpperNumeric;
     }
   
-    else if (upperConfirm === true) {
+    else if (upperConf && !numericConf && !specialConf) {
       return combineUpper;
     }
   
-    else if (numericConfirm === true && specialConfirm === true) {
-      return combineSpecial;
-    }
-  
-    else if (numericConfirm === true) {
-      return combineNumericSpecial;
-    }
-  
-    else if (specialCharacter === true) {
-      return combineNumericSpecial;
-    }
-  
-    else if (upperConfirm === false && numericConfirm === false && specialConfirm === false) {
+    else if (!upperConf && !numericConf && !specialConf) {
       return lowerCharacter;
     }
   
-    else if (specialConfirm === true && upperConfirm === false && numericCharacter === false) {
+    else if (!upperConf && !numericConf && specialConf) {
+      return combineSpecial;
+    }
+  
+    else if (!upperConf && numericConf && specialConf) {
       return combineNumericSpecial;
     }
-
-    return generatePassword();
   
+    else if (upperConf && numericConf && !specialConf) {
+      return combineUpperNumeric;
+    }
+
+    else { (!upperConf && numericConf && !specialConf);
+      return combineNumeric;
+    }
+
 };
   
-
 // Write password to the #password input
 function writePassword() {
     let password = generatePassword();
