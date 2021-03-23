4f41af698d35a6de118d0444d387acf2e5d9b432
David Bernard <dbernardcoding@gmail.com>
2021-03-23 15:58:00 -0400

    updating HTML, CSS, and images

---
+++ README.md
@@ -0,0 +1,33 @@
+# Secure Password Generator
+
+
+## Description
+
+This is a secure password generator built with HTML, CSS, and Javascript. The generator takes a users desired password length between 8 and 128 characters, and combines the users desired length with choices bewteen lowercase, uppercase, numeric, and spcecial characters. 
+
+
+## Usage
+
+To use the generator click the "Generate Password" button, which will prompt you to choose your desired password length and combination of characters. You must select a minimum of 8 characters and no more than 128. You will be prompted with four choices of characters to include, lowercase, uppercase, numeric, and special. You can choose as little as one set of characters or as many as four. The longer you make your password, and the more characters you choose the more secure your password will be. Once you are finished, the generator will display your secure password in the text area where you can copy it. [https://dbernard87.github.io/secure-password-generator/]
+
+![image of Secure Password Generator website homepage](assets/images/screenshot.png)
+
+
+## Features
+
+- Password length between 8 and 128 characters.
+- Up to four choices of characters for maximum security.
+- Flexbox for responsiveness.
+- Media queries for handheld devices.
+- Semantic HTML elements for SEO.
+- Clean, commented, and consolidated CSS and HTML files.
+
+
+## Credits
+
+- David Bernard : [https://github.com/dbernard87]
+- Background Photo by Markus Spiske from Pexels : [https://www.pexels.com/photo/green-and-yellow-printed-textile-330771/]
+- Icons from Font Awesome : [https://fontawesome.com/]
+- Stack Overflow : [https://stackoverflow.com/]
+- w3schools : [https://www.w3schools.com/]
+- w3resource : [https://www.w3resource.com/]
---
+++ assets/js/script.js
@@ -0,0 +1,119 @@
+// Global Variables
+let generateBtn = document.querySelector("#generate");
+let lowerCharacter = "abcdefghijklmnopqrstuvwxyz";
+let upperCharacter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let numericCharacter = "0123456789";
+let specialCharacter = "!#$%&()*+-.,/:;<=>?@[]^_`{|}~]";
+let countMessage = "";
+let lowerConf = ""; 
+let upperConf = "";
+let numericConf = ""; 
+let specialConf = "";
+let noCharacter = "";
+
+// Global Event Listeners
+generateBtn.addEventListener("click", characterLength); 
+generateBtn.addEventListener("click", writePassword); 
+
+// characterLength Function to choose length of password
+function characterLength() {
+  countMessage = prompt("Choose amount of characters between 8 and 128.");
+    if (countMessage < 8) {
+        alert("Must have at least 8 characters.");
+        characterLength();
+      } else if (countMessage > 128) {
+        alert("Must have no more than 128 characters.");
+        characterLength();
+      }
+      return countMessage;
+}
+
+// characterChoice Function
+function characterChoice() {
+  // Character prompt questions
+  lowerConf = confirm("Do you want to include lowercase characters?");
+  upperConf = confirm("Do you want to include uppercase characters?");
+  numericConf = confirm("Do you want to include numeric characters?");
+  specialConf = confirm("Do you want to include special characters?");
+  // Concatenated character strings for if else statement
+  let numericSpecial = numericCharacter.concat(specialCharacter);  
+  let combineAll = lowerCharacter.concat(upperCharacter, numericCharacter, specialCharacter);
+  let combineUpperNumeric = lowerCharacter.concat(upperCharacter, numericCharacter);
+  let combineUpper = lowerCharacter.concat(upperCharacter);
+  let combineUpperSpecial = lowerCharacter.concat(upperCharacter, specialCharacter);
+  let combineNumeric = lowerCharacter.concat(numericCharacter);
+  let combineSpecial = lowerCharacter.concat(specialCharacter);
+  let combineNumericSpecial = lowerCharacter.concat(numericCharacter, specialCharacter);
+  let upperNumeric = upperCharacter.concat(numericCharacter);
+  let upperSpecial = upperCharacter.concat(specialCharacter);
+  let upperNumericSpecial = upperCharacter.concat(numericCharacter, specialCharacter);
+      // If else statement to determine which character variable to choose
+      if (lowerConf && upperConf && numericConf && !specialConf) {
+      alert("Lowercase, uppercase, and numeric characters seleced.");
+      return combineUpperNumeric;
+    } else if (lowerConf && upperConf && !numericConf && !specialConf) {
+      alert("Lowercase and uppercase characters selected.");
+      return combineUpper;
+    } else if (lowerConf && !upperConf && !numericConf && specialConf) {
+      alert("Lowercase and special characters selected.");
+      return combineSpecial;
+    } else if (lowerConf && !upperConf && numericConf && specialConf) {
+      alert("Lowercase, numeric, and special characters selected.");
+      return combineNumericSpecial;
+    } else if (lowerConf && !upperConf && numericConf && !specialConf) {
+      alert("Lowercase and numeric characters selected.");
+      return combineNumeric;
+    } else if (!lowerConf && upperConf && numericConf && !specialConf) {
+      alert("Uppercase and numeric characters selected.");
+      return upperNumeric;
+    } else if (!lowerConf && !upperConf && numericConf && specialConf) {
+      alert("Numeric and special characters selected."); 
+      return numericSpecial;
+    } else if (!lowerConf && upperConf && numericConf && specialConf) {
+      alert("Uppercase, numeric, and special characters selected."); 
+      return upperNumericSpecial;
+    } else if (!lowerConf && !upperConf && !numericConf && !specialConf) {
+      alert("Must select at least one group of characters.");
+      return noCharacter;
+    } else if (lowerConf && upperConf && numericConf  && specialConf) {
+      alert("All characters selected.");
+      return combineAll;
+    } else if (lowerConf && !upperConf && !numericConf && !specialConf) {
+      alert("Lowercase characters selected.");
+      return lowerCharacter;
+    } else if (!lowerConf && upperConf && !numericConf && !specialConf) {
+      alert("Uppercase characters selected.");
+      return upperCharacter;
+    } else if (!lowerConf && !upperConf && numericConf && !specialConf) {
+      alert("Numeric characters selected.");
+      return numericCharacter; 
+    } else if (!lowerConf && !upperConf && !numericConf && specialConf) {
+      alert("Special characters selected.");
+      return specialCharacter;
+    } else if (!lowerConf && upperConf && !numericConf && specialConf) {
+      alert("Special and uppercase characters selected.");
+      return upperSpecial;
+    } else if (lowerConf && upperConf && !numericConf && specialConf) {
+      alert("Lowercase, uppercase, and special characters selected.");
+      return combineUpperSpecial;
+    }
+}
+
+// Combine characterLength and characterChoice Functions
+function generatePassword() {
+  let a = countMessage;
+  let b = characterChoice();
+  let passwordNew = "";
+  // For loop to make string length equal to user input
+  for(let i=0; i<a; i++) {
+   passwordNew = passwordNew + b.charAt(Math.floor(Math.random() * b.length));
+  }
+  return passwordNew;
+}
+  
+// Write password to the #password input
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
